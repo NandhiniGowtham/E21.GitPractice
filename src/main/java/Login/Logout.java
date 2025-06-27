c8468695e3a660c2a99318fd27c0db39b2e0e562
NandhiniGowtham <nandhinivenkat1129@gmail.com>
2025-06-26 14:43:33 -1200

    Logout class commited

---
+++ src/main/java/Login/Logout.java
@@ -0,0 +1,10 @@
+package Login;
+
+public class Logout {
+	public void logoutpage() 
+	{
+		System.out.println(" Logout successfully");
+
+	}
+	
+}
--- src/test/java/Sample/Demo.java
+++ src/test/java/Sample/Demo.java
@@ -1,8 +1,9 @@
 package Sample;
 
 import Login.LoginPage;
+import Login.Logout;
 
-public class Demo extends LoginPage {
+public class Demo {
 
 	public static void main(String[] args) {
 		
@@ -10,6 +11,8 @@ public class Demo extends LoginPage {
 		 
 		 LoginPage.login("NandhiniGowtham","12345678909");
 		 System.out.println(".....");
+		 Logout lo=new Logout();
+		 lo.logoutpage();
 
 	}
 
