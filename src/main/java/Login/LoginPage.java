f4de2d3a6c48004ab2d29b821696b2b884e1d42c
NandhiniGowtham <nandhinivenkat1129@gmail.com>
2025-06-26 13:21:24 -1200

    LoginPage is commited

---
+++ src/main/java/Login/LoginPage.java
@@ -0,0 +1,10 @@
+package Login;
+
+public class LoginPage {
+
+	public static void login(String username,String password)
+	{
+		System.out.println("your username is:"+username);
+		System.out.println("your password is:"+password);
+		}
+}
--- src/test/java/Sample/Demo.java
+++ src/test/java/Sample/Demo.java
@@ -1,11 +1,14 @@
 package Sample;
 
-public class Demo {
+import Login.LoginPage;
+
+public class Demo extends LoginPage {
 
 	public static void main(String[] args) {
 		
-		 System.out.println("Hello");
-		 // TODO Auto-generated method stub
+		 System.out.println("LoginPage");
+		 
+		 LoginPage.login("NandhiniGowtham","12345678909");
 
 	}
 
