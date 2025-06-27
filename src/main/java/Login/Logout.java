94e1d771c287c40b52d54ab2552b472daa66a6ec
NandhiniGowtham <nandhinivenkat1129@gmail.com>
2025-06-26 15:02:29 -1200

    logout class commited

--- src/main/java/Login/Logout.java
+++ src/main/java/Login/Logout.java
@@ -4,7 +4,9 @@ public class Logout {
 	public void logoutpage() 
 	{
 		System.out.println("...............");
+		
 		System.out.println(" Logout successfully");
+		System.out.println("Thank You");
 		System.out.println("...............");
 
 	}
